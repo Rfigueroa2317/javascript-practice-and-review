e7586b6c9455f473d9e892e108cd9a7e1cf26277
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-24 19:35:57 -0500

    examples

---
+++ js/FunctionsReview.js
@@ -0,0 +1,22 @@
+// function increment(x){
+//     return x + 1;
+// }
+//
+// var four = increment(3);
+// var six = increment(increment(increment(3)))
+//
+// console.log(increment(six));
+
+// var increment = function (x) {
+//     return x + 1;
+// };
+//
+// var two = increment(1);
+// console.log(two);
+
+function sum(a,b) {
+    var result = a + b;
+    return result;
+}
+
+console.log(sum(5,5))
