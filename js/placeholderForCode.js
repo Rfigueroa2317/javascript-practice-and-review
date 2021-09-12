0ddf1a96d7c818ca29d22876a7c14bc311bf8b93
ricardofigueroa <rfigueroa2317@gmail.com>
2021-09-12 15:25:52 -0500

    Today's push

---
+++ js/placeholderForCode.js
@@ -0,0 +1,46 @@
+// // THIS FILE IS JUST FOR CAPSTONE CODE HOLDER \\
+//
+// export default function Profile(info) {
+//     console.log("test test etet!!!!")
+//     return `<h1>Profile page!!!</h1>
+//             <h3 class="display-name">${info.displayName}</h3>
+//             <h3 class="firstname">${info.firstname}</h3>
+//             <h3 class="lastname">${info.lastname}</h3>
+//             <h3 class="email">${info.email}</h3>
+//
+// email: $("#email").val(),
+//             firstname: $("#firstname").val(),
+//             lastname: $("#lastname").val(),
+//             displayName: $("#displayName").val(),
+//             password: $("#password").val()
+//
+// `
+// }
+//
+// `
+// import createView from "../createView.js";
+//
+// export default function Profile(props) {
+//     console.log(props.user)
+//     if(props.user === null)
+//         createView("/login");
+//
+//     console.log("test")
+//     return `<h1>Welcome ${props.user.displayName}!</h1>
+// <h3>${props.user.firstname}</h3>
+// <h3>${props.user.lastname}</h3>
+// <h3>${props.user.email}</h3>
+//
+//
+//
+//     `
+// }
+//
+// export function ProfileEvent() {
+//     console.log("inside profile event");
+// }
+//
+//
+//          <h3>${props.user.firstname}!</h3>
+//  <h3>${props.user.lastname}!</h3>
+//  <h3>${props.user.email}!</h3>
